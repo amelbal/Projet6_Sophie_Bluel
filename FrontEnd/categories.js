8b02ab640a3b689969d3f2088e29ae3ae9566aae
mel59 <amelbal592@gmail.com>
2024-11-22 15:29:25 +0100

    :récupération du backend, ajout des filtres

--- FrontEnd/categories.js
+++ FrontEnd/categories.js
@@ -20,9 +20,37 @@ async function AfficherCategories(){
     const filtreDiv = document.querySelector("#filtres");
 
     for( let i = 0; i < categories.length; i++){
-        filtreDiv.innerHTML += `<span id='${categories[i].id}' class="btn">${categories[i].name}</span>`
+        filtreDiv.innerHTML += `<span id='${categories[i].id}' class="btn" onclick="RecupererImageParCategorie('${categories[i].id}')">${categories[i].name}</span>`
     }
 
 }
 
 AfficherCategories();
+
+async function RecupererImageParCategorie(idCategorie) {
+    document.querySelector(".gallery").innerHTML="";
+    let travauxElement = document.createElement("div");
+    travauxElement.setAttribute("id","gallery");
+    const reponse = await fetch("http://localhost:5678/api/works");
+    const travauxArchitecte = await reponse.json();
+     travauxElement = document.querySelector(".gallery");
+    for (let i = 0; i < travauxArchitecte.length; i++) {
+        console.log(idCategorie);
+        console.log(travauxArchitecte[i]);
+        if (idCategorie==0) {
+            travauxElement.innerHTML += `
+            <figure>
+                <img src="${travauxArchitecte[i].imageUrl}" alt="${travauxArchitecte[i].title}">
+                <figcaption>${travauxArchitecte[i].title}</figcaption>
+            </figure>`;
+        }
+    else if (travauxArchitecte[i].categoryId==idCategorie) {
+
+        travauxElement.innerHTML += `
+        <figure>
+            <img src="${travauxArchitecte[i].imageUrl}" alt="${travauxArchitecte[i].title}">
+            <figcaption>${travauxArchitecte[i].title}</figcaption>
+        </figure>`;
+    }
+    }  
+}
